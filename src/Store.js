45b8d97f796576a5276eb5237a2b7aa14de81d3f
Dipak Nimavat <112933488+dipak1950@users.noreply.github.com>
2023-03-03 15:47:36 +0530

    Add Data Redux To Lacal Storage (Auto)

--- src/Store.js
+++ src/Store.js
@@ -1,5 +1,17 @@
 import { createStore } from "redux";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 import reduce from "./Services/Reducer";
-const Store = createStore(reduce, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const persistConfig = {
+    key: 'myConfig',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reduce)
+
+const Store = createStore(persistedReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+
+export const persistor = persistStore(Store)
 export default Store;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Store from './Store';
+import Store, { persistor } from './Store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
   <Provider store={Store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
